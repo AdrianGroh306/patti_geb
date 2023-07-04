c3e0ae51f81b1242f927569bfba033ed3e2a5d9d
AdrianGroh <116496298+AdrianGroh306@users.noreply.github.com>
2023-07-05 00:48:58 +0200

    Implementing Tutorial stuff

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:patti_geb/barriers.dart';
 import 'package:patti_geb/caracter.dart';
 
 class HomePage extends StatefulWidget {
@@ -41,62 +41,77 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            flex: 3,
-            child: GestureDetector(
-              onTap: () {
-                if (gameHasStarted) {
-                  flyUp();
-                } else {
-                  startGame();
-                }
-              },
-              child: AnimatedContainer(
-                alignment: Alignment(0, caracYaxis), // x,y
-                color: Colors.grey,
-                duration: const Duration(milliseconds: 0),
-                child: const MyCaracter(),
-              ),
-            ),
-          ),
-          Container(
-            height: 15,
-            color: Colors.black,
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.brown,
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return GestureDetector(
+      onTap: () {
+        if (gameHasStarted) {
+          flyUp();
+        } else {
+          startGame();
+        }
+      },
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Stack (
                 children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text("SCORE",style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),),
-                      SizedBox(height: 10,),
-                      Text("0",style: TextStyle(color: Colors.white,fontSize: 25,fontWeight: FontWeight.bold),),
-                      SizedBox(height: 25,),
-                    ],
+                  AnimatedContainer(
+                    alignment: Alignment(0, caracYaxis), // x,y
+                    color: Colors.grey,
+                    duration: const Duration(milliseconds: 0),
+                    child: const MyCaracter(),
                   ),
-
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text("BEST",style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),),
-                      SizedBox(height: 10,),
-                      Text("10",style: TextStyle(color: Colors.white,fontSize: 25,fontWeight: FontWeight.bold),),
-                      SizedBox(height: 25,),
-                    ],
+                  Container(
+                    alignment: const Alignment(0,-0.3),
+                    child:gameHasStarted ? Text("") : Text("TAP TO PLAY",style: TextStyle(fontSize: 18,color: Colors.white),),
                   ),
-
+                  AnimatedContainer(
+                    alignment: Alignment(0,1.1),
+                    duration: Duration(milliseconds: 0),
+                    child: MyBarrier(
+                      size: 200.0,
+                    ),
+                  ),
                 ],
+              )
+            ),
+            Container(
+              height: 15,
+              color: Colors.black,
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.brown,
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text("SCORE",style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),),
+                        SizedBox(height: 10,),
+                        Text("0",style: TextStyle(color: Colors.white,fontSize: 25,fontWeight: FontWeight.bold),),
+                        SizedBox(height: 25,),
+                      ],
+                    ),
+
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text("BEST",style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),),
+                        SizedBox(height: 10,),
+                        Text("10",style: TextStyle(color: Colors.white,fontSize: 25,fontWeight: FontWeight.bold),),
+                        SizedBox(height: 25,),
+                      ],
+                    ),
+
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
