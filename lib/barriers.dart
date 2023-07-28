b736f161c9a04383f07e9f3526450b03ec7a9e77
AdrianGroh <116496298+AdrianGroh306@users.noreply.github.com>
2023-07-28 11:51:57 +0200

    barrier change

--- lib/barriers.dart
+++ lib/barriers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_glow/flutter_glow.dart';
 
 class MyBarrier extends StatelessWidget {
   final double barrierWidth;
@@ -23,18 +24,29 @@ class MyBarrier extends StatelessWidget {
         (2 * barrierX + barrierWidth) / (2 - barrierWidth),
         isThisBottomBarrier ? 1.1 : -1.1,
       ),
-      child: Container(
-        width: MediaQuery.of(context).size.width * barrierWidth / 1.8,
-        height: MediaQuery.of(context).size.height * 3 / 4 * barrierHeight / 2,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(backgroundImage),
-            fit: BoxFit.fill,
-          ),
+      child: PhysicalModel(
+        color: Colors.transparent,
+        shadowColor: Colors.white,
+        elevation: 5,
+        borderRadius: BorderRadius.circular(10),
+        child: GlowContainer(
+          // Use GlowContainer to wrap the existing barrier
+          glowColor: Colors.white, // Set the glow color to white
           borderRadius: BorderRadius.circular(10),
+          spreadRadius: 3, // Control the spread radius of the glow effect
+          child: Container(
+            width: MediaQuery.of(context).size.width * barrierWidth / 1.8,
+            height: MediaQuery.of(context).size.height * 3 / 4 * barrierHeight / 2,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(backgroundImage),
+                fit: BoxFit.fill,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
         ),
       ),
     );
   }
 }
-
